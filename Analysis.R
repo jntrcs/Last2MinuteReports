2cbc2dfa12eefb34dc23e377a620e11cea0a89ed
Jackson Curtis <jackson.curtis@podium.com>
2021-03-04 18:38:59 -0700

    Fixed the analysis to use the write csv and use mistakes per minute

--- Analysis.R
+++ Analysis.R
@@ -1,20 +1,26 @@
-calls= list.files("Games/") %>%
-  map_dfr(function(file){
-    read_csv(glue("Games/{file}")) %>%
-      pivot_wider(names_from = CallRatingName, values_from=N) %>%
-      mutate(game_id = substr(file, nchar(file)-13, nchar(file)-4))
-  }) %>%
-  select(game_id, everything(), -`NA`) 
+game_data = read_csv("GameReports.csv")
+ref_df = read_csv("AllRefsAllGames.csv")
 
-ref_scores=calls  %>% mutate(across(CC:IC, function(x)replace_na(x, 0)))%>% left_join(ref_df) %>%
+calls= game_data %>% group_by(game_id) %>%
+  summarize(minutes = length(unique(PeriodName))*2,
+            ICs = sum(CallRatingName=="IC")+sum(CallRatingName=="INC"),
+            CCs = sum(CallRatingName=="CC")+sum(CallRatingName=="CNC"))
+
+
+ref_scores=calls  %>% mutate(across(ICs:CCs, function(x)replace_na(x, 0)))%>% left_join(ref_df) %>%
   group_by(Ref) %>%
-  summarize(Games_with_Reports=n(), Missed_Calls = sum(INC)+sum(IC), 
+  summarize(Games_with_Reports=n(), 
+            Minutes = sum(minutes),
+            Missed_Calls = sum(ICs),
             #Missed_Calls_Per_Game = Missed_Calls/Games_with_Reports,
-            Total_Calls= sum(INC+IC+CC+CNC),
-            Total_Bad_Calls=sum(INC+IC),
-            Incorrect_Ratio=sum(INC+IC)/(sum(INC+IC+CC+CNC))) %>%
-  arrange(desc(Incorrect_Ratio)) %>%
+            Total_Calls= sum(ICs+CCs),
+            Incorrect_Ratio=Missed_Calls/Total_Calls,
+            Bad_Calls_Per_Minute = Missed_Calls/Minutes) %>%
+  arrange(desc(Bad_Calls_Per_Minute)) %>%
   mutate(League_Rank=nrow(.):1, Ref=fct_rev(fct_inorder(Ref)))
+
+ggplot(ref_scores, aes(x=Incorrect_Ratio, y=Bad_Calls_Per_Minute))+
+  geom_point()
 
 ref_scores %>% filter(Ref %in% our_refs )
 
@@ -22,10 +28,9 @@ ref_scores %>% filter(Ref %in% our_refs )
 our_refs = c("Sean Corbin", "Michael Smith", "Brandon Adair")
 
 ref_scores %>%
-  ggplot(aes(y=Ref, x=Incorrect_Ratio))+geom_col(aes(fill=Ref%in%our_refs))+
+  ggplot(aes(y=Ref, x=Bad_Calls_Per_Minute))+geom_col(aes(fill=Ref%in%our_refs))+
   ggtitle("Ref by Last Two Minute Performance")+
-  scale_x_continuous(labels=scales::label_percent(1))+
-  xlab("Percent of calls deemed incorrect on last two minute report")+
+  xlab("Bad calls / minute")+
   guides(fill=guide_legend(title="Refs in Jazz/Sixers Game"))+
   scale_fill_manual(values=c("forestgreen", "red"), labels=c("No", "Yes"))+
   theme_minimal()+
